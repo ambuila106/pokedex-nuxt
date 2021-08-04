91c2165e5dea47c7e49b14ea66cda90281358cd2
ambuila106 <brandon.ambuila@unillanos.edu.co>
2021-08-03 22:43:45 -0500

    added sass loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <Tutorial/>
-</template>
+  <div>
+    <h1>welcome to Pokédex</h1>
+    <p>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</p>
+  </div>
+  </template>
 
 <script lang="ts">
 import Vue from 'vue'

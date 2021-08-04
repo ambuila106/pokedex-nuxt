21702288a01ef443c0b3509b14e9cfe9c93ad4da
ambuila106 <brandon.ambuila@unillanos.edu.co>
2021-08-04 00:16:46 -0500

    Implemented index design

---
+++ pages/Pokedex.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    pokedex
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <h1>welcome to Pokédex</h1>
-    <p>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</p>
+  <div class="container-wrap flex-center text-center">
+    <div>
+      <img src="/pikachu-circle.svg" alt="Pikachu Hello" />
+      <h2>Welcome to Pokédex</h2>
+      <span>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</span>
+      <div style="margin-top: 3.125rem">
+        <nuxt-link class="btn" to="/pokedex">Get Started</nuxt-link>
+      </div>
+    </div>
+
   </div>
   </template>
 
